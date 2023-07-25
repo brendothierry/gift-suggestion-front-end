8b47983a5d66e0d244486f2b404e7ce30f304999
Brendo Thierry <poxabrendo@hotmail.com>
2023-07-25 01:34:33 -0300

    Criando o primeiro componente de NavBar, ainda não está finalizado

---
+++ src/components/MenuData.js
@@ -0,0 +1,7 @@
+export const MenuData = [
+    {
+        url: "#",
+        cName: "navMenu",
+        icon:"fa-solid fa-bars"
+    }
+]
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import { Component } from "react";
+import logo from '../Images/logo.svg';
+import { MenuData } from "./MenuData";
+import "../css/NavbarStyles.css";
+
+class Navbar extends Component {
+    state= {clicked: false};
+    handleClick =()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <div className="navMenu" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fa-solid fa-bars"}></i>
+                </div>
+                <ul className="nav-menu">
+                    <navMenu>
+                        {/* {MenuData.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={item.url}
+                                    className={item.cName}>
+                                    <i className={item.icon}>
+                                    </i>{item.title}
+                                </a>
+                            </li>
+                        )
+                    })} */}
+                    </navMenu>
+                </ul>
+                <div className="logo-container">
+                    {/* <i class="fa-solid fa-arrow-left-long" style={{ color: '#15154c' }}></i> */}
+                    <img className="logo" src={logo} />
+                </div>
+
+            </nav>
+        );
+    }
+}
+export default Navbar;
