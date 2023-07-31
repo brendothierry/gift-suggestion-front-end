4ea7c6a7e32c4a7e1a4db1d6f510111d83105366
Brendo Thierry <poxabrendo@hotmail.com>
2023-07-31 19:33:14 -0300

    Incluindo alterações do reikid por causa de conflitos

---
+++ src/components/InputLogin/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import "./styles.css"
+
+
+class InputLogin extends React.Component {
+    state = {
+        email: '',
+        senha: ''
+    }
+
+    render() {
+        return (
+            <div className="container"> 
+                <div title="login">
+                    <div className="login">
+                        <div label="Email: * " htmlFor="exempleInputEmail1">
+                            <input 
+                                type="email"
+                                value={this.state.email}
+                                onChange={e => this.setState({ email: e.target.value })}
+                                id="exampleInputEmail11"
+                                aria-describedby="emailHelp"
+                                placeholder="Username" />
+                        </div>
+                        <br></br>
+                        <div label="Senha : * " htmlFor="">
+                            <input 
+                                type="password"
+                                value={this.state.senha}
+                                onChange={e => this.setState({ senha: e.target.value })}
+                                id="exampleInputPassword1"
+                                placeholder="Password" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default InputLogin
