b39842cff9a605ffc4e33de4c47ec3a2769a09b0
Brendo Thierry <poxabrendo@hotmail.com>
2023-08-08 11:52:19 -0300

    Fix App

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import MainRoute from './router';
 import Home from '../views/home';
 import Header from '../components/Header';
+import Login from '../views/Login';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <MainRoute/>
+        <Login/>
       </div>
     )
   }
--- src/main/router.js
+++ src/main/router.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Home from '../views/home'
 
 import { Route, Routes, HashRouter } from 'react-router-dom'
+import Login from '../views/Login';
 
 function MainRoute(){
     return(
         <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/login" element={<Login />}/>
             </Routes>
         </HashRouter>
     )   
