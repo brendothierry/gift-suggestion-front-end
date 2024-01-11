31cb4966f01ca6e8347071ef017eafae8b807e56
Brendo Thierry <49412438+brendothierry@users.noreply.github.com>
2024-01-11 02:34:54 -0300

    Revert "Dev thierry"

--- src/components/GiftSuggestionModel/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-const AppContainer = ({ children }) => {
-  return <div className="app-container">{children}</div>;
-};
-
-export default AppContainer;
